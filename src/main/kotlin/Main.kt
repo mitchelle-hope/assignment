68697f83f768012f203f3f5e5b741dc7bc73f4c3
mitchelle-hope <mitchellehopewaithera@gmail.com>
2022-02-12 10:20:13 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    var name = "mitchelle hope"
+    println(name)
+    var age = "23"
+    println(age)
+    var weight = "75"
+    println(weight)
+    var phonenumber = "0716707145"
+    println(phonenumber)
+    val iskenyan: Boolean = true
+    println(iskenyan) // output true
+    val isnotkenyan: Boolean = false
+    println(isnotkenyan) // output false
+}
+
